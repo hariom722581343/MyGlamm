04a7e6f78df78c49e4ea4b64308a5ff6e786f972
sumitbadri2000 <sumitbadri2000@gmail.com>
2022-11-11 17:42:27 +0530

    day-4

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ import { navbar } from "./components/navbar.js";
 let Navbar=document.getElementById("nav");
 Navbar.innerHTML=navbar();
 
-<<<<<<< HEAD
-// import { footer } from "./components/footer.js";
-
-// let footer=document.getElementById("footer");
-// Navbar.innerHTML=footer();
-=======
 import { footer } from "./components/footer.js";
 let foot=document.getElementById("footer");
 foot.innerHTML=footer();
->>>>>>> 8a01c86fa1835295c7b32f43a46a8573d5c46cae
 
 let slider=document.getElementById("slider");
 let slide=["https://files.myglamm.com/site-images/original/Homepage-desktop---1920-x-527---CTA-yes_8.jpeg","https://files.myglamm.com/site-images/original/popxo1920x527.gif","https://files.myglamm.com/site-images/original/Manish1920x527.jpg","https://files.myglamm.com/site-images/original/Homepage-1920x527_3.jpeg"];
